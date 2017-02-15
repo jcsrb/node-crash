055ec432eafc1e8a394bf81c6d5977757dd05dd2
Jakob Cosoroabă <jakob@cosoroaba.ro>
2017-02-15 15:27:47 +0200

    Update crash.cc

--- src/crash.cc
+++ src/crash.cc
@@ -11,18 +11,7 @@ using v8::Value;
 
 void Method(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
-  char a [1];
-    //Create a index
-    int i = 0;
-    //Infinite loop to go around the compiler
-    while(1)
-    {
-        //Write on case i of a, on the second iteration, it will write in unreserved memory => crash
-        a[i] = 0;
-        i = i + 1;
-    }
   throw "Crash!";
-  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
 }
 
 void init(Local<Object> exports) {
